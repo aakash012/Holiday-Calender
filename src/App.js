706d1b2ad3a2b238c51c970ef66f35f6d0fd0bdf
aakash012 <aakash012@live.com>
2019-07-27 18:15:14 +0530

    Components added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,74 @@
-import React,{Component} from 'react';
-import Homepage from './layout/homepage/Homepage';
+import React, {useEffect, useState} from 'react';
+import './App.css';
+import Today from "./Today";
+import Upcoming from "./Upcoming";
+import Past from "./Past";
 
-class App extends Component{
-  render(){
-    return(
-      <Homepage />
-    );
-  }
+const st = {
+    boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+};
+
+function App() {
+    const [holidays, setHolidays] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [mode, setMode] = useState(1);
+    const [error, setError] = useState(false)
+
+    function clickHandler(mode1) {
+        setMode(mode1);
+    }
+
+    useEffect(() => {
+        const url = 'https://calendarific.com/api/v2/holidays?country=IN&year=2019&api_key=2e3b6f568fe3cd7da7bdb937d45613ad41c3f347&amp';
+        fetch(url)
+            .then(res => res.json())
+            .then((result) => {
+                    if (result.meta.code === 200) {
+                        setLoading(false)
+                        setHolidays(result.response.holidays);
+                    }
+                },
+                (error) => {
+                    setLoading(false)
+                    setError(true)
+                    console.log('error')
+                }
+            )
+    }, []);
+
+    if (error)
+        return <h3>Error Loading Data</h3>;
+    else
+        return (
+            <div>
+                {loading ? (
+                    <h3>Loading...</h3>
+                ) : (
+                    <>
+                        <br/>
+                        <Today holidays={holidays}/>
+                        <br/>
+                        <div className="btn-group btn-group-lg" role='group' aria-label='btn-grp1' style={st}>
+
+                            <button type="button" className={!mode ? "btn btn-outline-primary" : "btn btn-outline-primary active"}
+                                    onClick={() => clickHandler(1)}>Upcoming
+                                Holidays
+                            </button>
+                            <button type="button" className={mode ? "btn btn-outline-primary" : "btn btn-outline-primary active"}
+                                    onClick={() => clickHandler(0)}>Passed
+                                Holidays
+                            </button>
+                        </div>
+                        {mode ? (
+                                <Upcoming holidays={holidays}/>
+                            ) :
+                            (<Past holidays={holidays}/>)
+                        }
+
+                    </>
+                )}
+            </div>
+        );
 }
+
 export default App;
